a397eace00e9f0bedd4b5f2d60161b948bfc2e3d
Leandro <leandrocasavalle10@gmail.com>
2023-05-18 18:44:37 -0300

    Commit del punto 1

---
+++ practico6/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+*/
+Console.WriteLine("Ingrese un numero:");
+string A = Console.ReadLine();
+int a =Int32.Parse(A);
+int inv;
+int invdo = 0;
+while(a>=10){
+    inv = a%10;
+    a=a/10;
+    invdo = invdo*10 + inv;
+}
+
+invdo = invdo*10 + a;
+
+
+Console.WriteLine("valor de a invertido:"+invdo);
