b6479a7c2013597e505cf5f930b2d594a7f7698e
William24A <willi24194@gmail.com>
2023-05-18 19:38:21 -0300

    commit ejercicio 2

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -9,7 +9,7 @@ b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
 */
-Console.WriteLine("Ingrese un numero positivo:");
+/*Console.WriteLine("Ingrese un numero positivo:");
 int a;
 bool control = int.TryParse(Console.ReadLine(), out a);
 int inv;
@@ -26,4 +26,39 @@ if(control){
 
 
         Console.WriteLine("valor de a invertido:"+invdo);
-}
+}*/
+do{
+    Console.WriteLine("Elija una opcion");
+    Console.WriteLine("1-Suma");
+    Console.WriteLine("2-Resta");
+    Console.WriteLine("3-Producto");
+    Console.WriteLine("4-Division");
+    Console.WriteLine("5-Salir");
+    Console.WriteLine("Ingrese una opcion:");
+    int a;
+    float A, B;
+    bool control = int.TryParse(Console.ReadLine(), out a);
+    if(control){
+            switch(a){
+            case 1:
+                Console.ReadKey();
+                break;
+            case 2:
+                Console.ReadKey();
+                break;
+            case 3:
+                Console.ReadKey();
+                break;
+            case 4:
+                Console.ReadKey();
+                break;
+            default:
+                Console.WriteLine("Muchas gracias por elegirnos.");
+                break;
+            }
+    }else{
+        Console.WriteLine("No ingreso un numero. Presione una tecla para continuar.");
+        Console.ReadKey();
+    }
+}while( a < 5);
+
