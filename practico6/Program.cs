7ea06d9617e8704eb9448e18e0f1945f0dd05fd5
William <willi241194@gmail.com>
2023-05-20 23:08:37 -0300

    Commit punto 3 de la rama 2

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -35,7 +35,9 @@ do{
     Console.WriteLine("2-Resta");
     Console.WriteLine("3-Producto");
     Console.WriteLine("4-Division");
-    Console.WriteLine("5-Salir");
+    Console.WriteLine("5-Datos de un número");
+    Console.WriteLine("6-Maximo y minimo");
+    Console.WriteLine("7-Salir");
     Console.WriteLine("Ingrese una opcion:");
 
     float A, B;//vaiables a sumar,restar,dividir o multiplicar
@@ -43,42 +45,75 @@ do{
     if(control){
             switch(a){
             case 1:
-                Console.WriteLine("Ingrese los valores a sumar: ")
+                Console.WriteLine("Ingrese los valores a sumar: ");
                 //en este caso usamos una cadena para ingresar un float
                 if(float.TryParse(Console.ReadLine(), out A) && float.TryParse(Console.ReadLine(), out B) ){
                     Console.WriteLine(A+B);
                 }else{
-                    Console.WriteLine("Debe ingresar numeros.")
+                    Console.WriteLine("Debe ingresar numeros.");
                 }
                 Console.WriteLine("Presione enter para continuar.");
                 Console.ReadKey();
                 break;
             case 2:
-                Console.WriteLine("Ingrese los valores a restar: ")
+                Console.WriteLine("Ingrese los valores a restar: ");
                 if(float.TryParse(Console.ReadLine(), out A) && float.TryParse(Console.ReadLine(), out B) ){
                     Console.WriteLine(A-B);
                 }else{
-                    Console.WriteLine("Debe ingresar numeros.")
+                    Console.WriteLine("Debe ingresar numeros.");
                 }
                 Console.WriteLine("Presione enter para continuar.");
                 Console.ReadKey();
                 break;
             case 3:
-                Console.WriteLine("Ingrese los valores a multiplicar: ")
+                Console.WriteLine("Ingrese los valores a multiplicar: ");
                 if(float.TryParse(Console.ReadLine(), out A) && float.TryParse(Console.ReadLine(), out B) ){
                     Console.WriteLine(A*B);
                 }else{
-                    Console.WriteLine("Debe ingresar numeros.")
+                    Console.WriteLine("Debe ingresar numeros.");
                 }
                 Console.WriteLine("Presione enter para continuar.");
                 Console.ReadKey();
                 break;
             case 4:
-                Console.WriteLine("Ingrese los valores a dividir: ")
+                Console.WriteLine("Ingrese los valores a dividir: ");
                 if(float.TryParse(Console.ReadLine(), out A) && float.TryParse(Console.ReadLine(), out B) ){
                     Console.WriteLine(A/B);
                 }else{
-                    Console.WriteLine("Debe ingresar numeros.")
+                    Console.WriteLine("Debe ingresar numeros.");
+                }
+                Console.WriteLine("Presione enter para continuar.");
+                Console.ReadKey();
+                break;
+            case 5:
+                Console.WriteLine("Ingrese un número:");
+                if(float.TryParse(Console.ReadLine(), out A)){
+                    Console.WriteLine("El valor absoluto de "+A+" es: "+Math.Abs(A));
+                    Console.WriteLine("El cuadrado del numero "+A+" es: "+Math.Pow(A,2));
+                    if(A>=0){
+                        Console.WriteLine("La raiz del numero "+A+" es: "+Math.Sqrt(A));
+                    }
+                    Console.WriteLine("El valor del seno es: "+Math.Asin(A));
+                    Console.WriteLine("El valor del coseno es: "+Math.Acos(A));
+                    Console.WriteLine("El valor entero es: "+(int)Math.floor(A));
+                }else{
+                    Console.WriteLine("Debe ingresar numeros.");
+                }
+                Console.WriteLine("Presione enter para continuar.");
+                Console.ReadKey();
+                break;
+            case 6:
+                Console.WriteLine("Ingrese los valores a comparar: ")
+                if(float.TryParse(Console.ReadLine(), out A) && float.TryParse(Console.ReadLine(), out B) ){
+                    if(A>=B){
+                        Console.WriteLine("El maximo es: "+A);
+                        Console.WriteLine("El minimo es: "+B);
+                    }else{
+                        Console.WriteLine("El maximo es: "+B);
+                        Console.WriteLine("El minimo es: "+A);
+                    }
+                }else{
+                    Console.WriteLine("Debe ingresar numeros.");
                 }
                 Console.WriteLine("Presione enter para continuar.");
                 Console.ReadKey();
